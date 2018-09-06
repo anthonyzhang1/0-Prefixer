aa4b32ce2a6e508c062338689022245e80138bc0
Anthony Zhang <42044884+anthonyzhang1@users.noreply.github.com>
2018-09-06 14:36:19 -0700

    Add files via upload

---
+++ FG #8 '0 Prefixer'.py
@@ -0,0 +1,19 @@
+#! python3
+# rename.py - Adds "0" to the beginning of the filename of all
+# .txt files in the current working directory.
+
+import os
+
+# Lists all files in the current working directory.
+cwd = os.listdir(os.getcwd())
+
+# Adds 0 before the filename for .txt files.
+for file in cwd:
+    if not file.endswith('.txt'):
+        continue
+
+    origname = file
+    newname = '0' + origname
+
+    # Commented for safety. Uncomment to use.
+    # os.rename(origname, newname)
